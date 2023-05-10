8989c43ece1a010bdec5ba6d56d929af167ea32f
cinderjk <cinderjk@gmail.com>
2023-05-10 10:40:59 +0700

    Create migration, factory, seeders users table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(UserSeeder::class);
+        User::factory()->create();
     }
 }
