087eb3e666cd0d92f52fdba8def41f1ee8701227
cinderjk <cinderjk@gmail.com>
2023-05-10 09:35:12 +0700

    Fresh install command

---
+++ app/Console/Commands/FreshInstall.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class FreshInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fresh:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Doing fresh install for the application';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Doing migration...');
+        Artisan::call('migrate:fresh');
+        $this->info('Doing seeding...');
+        Artisan::call('db:seed');
+        $this->info('Done!');
+    }
+}
